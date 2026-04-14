dd5aa7bd4a1f9c3d9fea89cf1034210ba2fe2354
pial6215 <pial15-6215@s.diu.edu.bd>
2026-04-14 13:26:52 +0600

    Search option added and UI improved on Home page

--- web_eng_final_project/listings/views.py
+++ web_eng_final_project/listings/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q  # Eita lagbe search er jonno
 from .models import Listing
 from .forms import ListingForm 
 
 def home_view(request):
-    listings = Listing.objects.all().order_by('-created_at')
+    query = request.GET.get('search') 
+    if query:
+        # Title ba Location jekonota match korlei result dekhabe
+        listings = Listing.objects.filter(
+            Q(title__icontains=query) | Q(location__icontains=query)
+        ).order_by('-created_at')
+    else:
+        listings = Listing.objects.all().order_by('-created_at')
+        
     return render(request, 'listings/home.html', {'listings': listings})
-
 
 @login_required(login_url='/accounts/login/') 
 def create_listing(request):
     if request.method == 'POST':
         form = ListingForm(request.POST)
         if form.is_valid():
-            
             listing = form.save(commit=False)
-            
             listing.host = request.user 
             listing.save() 
             return redirect('home') 
     else:
         form = ListingForm()
     
+    # Apnar file name anujayi Notesing.html-e thaklo
     return render(request, 'listings/Notesing.html', {'form': form})
