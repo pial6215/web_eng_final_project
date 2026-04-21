fc222cde5b3616afd29903525eff013c27c85cd4
pial6215 <pial15-6215@s.diu.edu.bd>
2026-04-21 23:35:34 +0600

    Settings updated for hosting and requirements added

--- web_eng_final_project/mycore/settings.py
+++ web_eng_final_project/mycore/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-my-super-secret-key-for-this-project'
 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
